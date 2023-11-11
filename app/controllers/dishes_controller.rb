013befce745ab7d03ea0efc64685c297f891f4a0
Nathan Trautenberg <Natester123@mac.com>
2023-11-10 18:51:50 -0700

    deleted unnecessary update method from dishes controller.

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,16 +3,4 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
-
-  def update
-    @dish = Dish.find(params[:id])
-    if params[:delete_ingredient].present?
-      @ingredient = Ingredient.find(params[:delete_ingredient])
-      @dish.ingredients.delete(@ingredient)
-    else
-      @ingredient = Ingredient.find(params[:ingredient_id])
-      @dish.ingredients << @ingredient
-    end
-    redirect_to "/dishes/#{@dish.id}"
-  end
 end
